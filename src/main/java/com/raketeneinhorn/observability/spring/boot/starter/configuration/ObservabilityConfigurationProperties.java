129c4c48232b2a8f3cee39c189daf825b5f7ec1f
Raketeneinhorn GmbH <account+github@raketeneinhorn.com>
2024-08-08 21:50:58 +0200

    ci(sonar): configure coverage on sonarcloud.io

--- src/main/java/com/raketeneinhorn/observability/spring/boot/starter/configuration/ObservabilityConfigurationProperties.java
+++ src/main/java/com/raketeneinhorn/observability/spring/boot/starter/configuration/ObservabilityConfigurationProperties.java
@@ -1,12 +1,18 @@
 package com.raketeneinhorn.observability.spring.boot.starter.configuration;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import static com.raketeneinhorn.observability.spring.boot.starter.configuration.ObservabilityConfigurationProperties.PREFIX;
 
+@Getter
+@Setter
 @ConfigurationProperties(prefix = PREFIX)
 public class ObservabilityConfigurationProperties {
 
     public static final String PREFIX = "raketeneinhorn.observability";
+
+    private String foo;
 
 }
