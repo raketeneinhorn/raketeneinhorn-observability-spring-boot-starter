bc74a1531be7bc20879b6dff7685ba0e2340dbb7
Raketeneinhorn GmbH <account+github@raketeneinhorn.com>
2024-08-13 19:31:41 +0200

    feat: configure response handler and starter

--- src/main/java/com/raketeneinhorn/observability/spring/boot/starter/configuration/ObservabilityConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.raketeneinhorn.observability.spring.boot.starter.configuration;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties(ObservabilityConfigurationProperties.class)
-public class ObservabilityConfiguration {
-}
