9cdeee8dc6604945c14c196d601fb080035773d7
Raketeneinhorn GmbH <account+github@raketeneinhorn.com>
2024-08-13 20:16:58 +0200

    refactor: use unique class name

--- src/main/java/com/raketeneinhorn/observability/spring/boot/starter/exception/ResponseEntityExceptionHandler.java
+++
@@ -1,8 +0,0 @@
-package com.raketeneinhorn.observability.spring.boot.starter.exception;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-
-@ControllerAdvice
-public class ResponseEntityExceptionHandler extends org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler {
-
-}
--- src/main/java/com/raketeneinhorn/observability/spring/boot/starter/exception/configuration/ObservabilityExceptionConfiguration.java
+++ src/main/java/com/raketeneinhorn/observability/spring/boot/starter/exception/configuration/ObservabilityExceptionConfiguration.java
@@ -1,10 +1,10 @@
 package com.raketeneinhorn.observability.spring.boot.starter.exception.configuration;
 
-import com.raketeneinhorn.observability.spring.boot.starter.exception.ResponseEntityExceptionHandler;
+import com.raketeneinhorn.observability.spring.boot.starter.exception.SimpleResponseEntityExceptionHandler;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackageClasses = ResponseEntityExceptionHandler.class)
+@ComponentScan(basePackageClasses = SimpleResponseEntityExceptionHandler.class)
 public class ObservabilityExceptionConfiguration {
 }
