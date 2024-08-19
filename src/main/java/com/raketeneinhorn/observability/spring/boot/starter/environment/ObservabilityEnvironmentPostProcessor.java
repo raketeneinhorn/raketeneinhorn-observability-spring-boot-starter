52de003f9384cc5d23e3ebf68b786fcb964ecbd3
Raketeneinhorn GmbH <account+github@raketeneinhorn.com>
2024-08-19 21:16:28 +0200

    feat: configure tracing propability

--- src/main/java/com/raketeneinhorn/observability/spring/boot/starter/environment/ObservabilityEnvironmentPostProcessor.java
+++ src/main/java/com/raketeneinhorn/observability/spring/boot/starter/environment/ObservabilityEnvironmentPostProcessor.java
@@ -14,12 +14,16 @@ import java.util.Map;
 public class ObservabilityEnvironmentPostProcessor implements EnvironmentPostProcessor {
 
     protected static final String BANNER_MODE_PROPERTY_KEY = "spring.main.banner-mode";
+    protected static final String TRACING_SAMPLING_PROPABILITY_PROPERTY_KEY = "management.tracing.sampling.probability";
 
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
         PropertySource<Map<String,Object>> mapPropertySource = new MapPropertySource(
             ObservabilityEnvironmentPostProcessor.class.getName(),
-            Map.of(BANNER_MODE_PROPERTY_KEY, Banner.Mode.OFF)
+            Map.of(
+                BANNER_MODE_PROPERTY_KEY, Banner.Mode.OFF,
+                TRACING_SAMPLING_PROPABILITY_PROPERTY_KEY, 0.1F
+            )
         );
         environment.getPropertySources().addLast(mapPropertySource);
     }
