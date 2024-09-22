557e1c43f914d102ecb01143c1ed56d7553a43d5
Raketeneinhorn GmbH <account+github@raketeneinhorn.com>
2024-09-22 22:45:09 +0200

    fix: preserve existing environment entries

--- src/main/java/com/raketeneinhorn/observability/spring/boot/starter/environment/ObservabilityEnvironmentPostProcessor.java
+++ src/main/java/com/raketeneinhorn/observability/spring/boot/starter/environment/ObservabilityEnvironmentPostProcessor.java
@@ -14,15 +14,21 @@ import java.util.Map;
 public class ObservabilityEnvironmentPostProcessor implements EnvironmentPostProcessor {
 
     protected static final String BANNER_MODE_PROPERTY_KEY = "spring.main.banner-mode";
+    private static final Banner.Mode BANNER_MODE_DEFAULT_VALUE = Banner.Mode.OFF;
+
     protected static final String TRACING_SAMPLING_PROBABILITY_PROPERTY_KEY = "management.tracing.sampling.probability";
+    private static final Float TRACING_SAMPLING_PROBABILITY_DEFAULT_VALUE = 1.0F;
 
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        Banner.Mode bannerMode = environment.getProperty(BANNER_MODE_PROPERTY_KEY, Banner.Mode.class, BANNER_MODE_DEFAULT_VALUE);
+        Float tracingSamplingProbability = environment.getProperty(TRACING_SAMPLING_PROBABILITY_PROPERTY_KEY, Float.class, TRACING_SAMPLING_PROBABILITY_DEFAULT_VALUE);
+
         PropertySource<Map<String,Object>> mapPropertySource = new MapPropertySource(
             ObservabilityEnvironmentPostProcessor.class.getName(),
             Map.of(
-                BANNER_MODE_PROPERTY_KEY, Banner.Mode.OFF,
-                TRACING_SAMPLING_PROBABILITY_PROPERTY_KEY, 0.1F
+                BANNER_MODE_PROPERTY_KEY, bannerMode,
+                TRACING_SAMPLING_PROBABILITY_PROPERTY_KEY, tracingSamplingProbability
             )
         );
         environment.getPropertySources().addLast(mapPropertySource);
