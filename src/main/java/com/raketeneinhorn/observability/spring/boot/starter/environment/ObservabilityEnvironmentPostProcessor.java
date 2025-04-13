5508132018c40abc11774d5eca6ae42859ad0c3e
Raketeneinhorn GmbH <account+github@raketeneinhorn.com>
2025-04-13 14:33:24 +0200

    feat: add defaults for structured logging

--- src/main/java/com/raketeneinhorn/observability/spring/boot/starter/environment/ObservabilityEnvironmentPostProcessor.java
+++ src/main/java/com/raketeneinhorn/observability/spring/boot/starter/environment/ObservabilityEnvironmentPostProcessor.java
@@ -3,35 +3,69 @@ package com.raketeneinhorn.observability.spring.boot.starter.environment;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.boot.logging.structured.CommonStructuredLogFormat;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.env.PropertySource;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
+
+import static java.util.function.Predicate.not;
 
 @Order
 public class ObservabilityEnvironmentPostProcessor implements EnvironmentPostProcessor {
 
     protected static final String BANNER_MODE_PROPERTY_KEY = "spring.main.banner-mode";
-    private static final Banner.Mode BANNER_MODE_DEFAULT_VALUE = Banner.Mode.OFF;
-
     protected static final String TRACING_SAMPLING_PROBABILITY_PROPERTY_KEY = "management.tracing.sampling.probability";
-    private static final Float TRACING_SAMPLING_PROBABILITY_DEFAULT_VALUE = 1.0F;
+    protected static final String LOGGING_STRUCTURED_FORMAT_CONSOLE_PROPERTY_KEY = "logging.structured.format.console";
+
+    List<EnvironmentSpec<?>> environmentSpecs = List.of(
+        new EnvironmentSpec<>(BANNER_MODE_PROPERTY_KEY, Banner.Mode.class, Banner.Mode.OFF, e -> true),
+        new EnvironmentSpec<>(TRACING_SAMPLING_PROBABILITY_PROPERTY_KEY, Float.class, 1.0F, e -> true),
+        new EnvironmentSpec<>(LOGGING_STRUCTURED_FORMAT_CONSOLE_PROPERTY_KEY, CommonStructuredLogFormat.class, CommonStructuredLogFormat.LOGSTASH, not(this::isLocalEnvironment))
+
+    );
 
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
-        Banner.Mode bannerMode = environment.getProperty(BANNER_MODE_PROPERTY_KEY, Banner.Mode.class, BANNER_MODE_DEFAULT_VALUE);
-        Float tracingSamplingProbability = environment.getProperty(TRACING_SAMPLING_PROBABILITY_PROPERTY_KEY, Float.class, TRACING_SAMPLING_PROBABILITY_DEFAULT_VALUE);
+        Map<String, Object> mapSource = new HashMap<>();
+
+        environmentSpecs.stream()
+            .filter(es -> es.includeForEnvironment().test(environment))
+            .peek(es -> System.out.println("applying " + es))
+            .forEach(es -> mapSource.put(
+                es.key(),
+                es.effiectiveValueForEnvironment(environment)
+            ));
 
         PropertySource<Map<String,Object>> mapPropertySource = new MapPropertySource(
             ObservabilityEnvironmentPostProcessor.class.getName(),
-            Map.of(
-                BANNER_MODE_PROPERTY_KEY, bannerMode,
-                TRACING_SAMPLING_PROBABILITY_PROPERTY_KEY, tracingSamplingProbability
-            )
+            mapSource
         );
         environment.getPropertySources().addLast(mapPropertySource);
+    }
+
+    private boolean isLocalEnvironment(Environment environment) {
+        return Arrays.stream(environment.getActiveProfiles()).anyMatch("local"::equalsIgnoreCase);
+    }
+
+    private record EnvironmentSpec<T>(
+        String key,
+        Class<T> targetType,
+        T defaultValue,
+        Predicate<Environment> includeForEnvironment
+    ) {
+
+        private T effiectiveValueForEnvironment(Environment environment) {
+            return environment.getProperty(key, targetType, defaultValue);
+        }
+
     }
 
 }
